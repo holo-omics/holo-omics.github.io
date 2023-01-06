7db113fdbf08d1b6da1612fa28b8c0b79cc837fd
Antton Alberdi <anttonalberdi@gmail.com>
2023-01-06 06:15:29 +0100

    Updates

--- render_book.R
+++ render_book.R
@@ -1,10 +1,10 @@
 library(bookdown)
 
-#Render it as github pages
+#Render it as github pages ()
 render_book(input = "index.Rmd", output_format = "bookdown::gitbook")
 
-#Render it as github pages
-render_book(input = "index.Rmd", output_format = "bookdown::html_book")
+#Render it as github pages (customisable option)
+#render_book(input = "index.Rmd", output_format = "bookdown::html_book")
 
 #Render it as pdf
 render_book(input = "index.Rmd", output_format = "bookdown::pdf_book")
