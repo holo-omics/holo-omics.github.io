51ad01523662f41a786eff8f4c393ac8edd5af72
Antton Alberdi <anttonalberdi@gmail.com>
2023-01-10 11:55:42 +0100

    Study design commit

--- render_book.R
+++ render_book.R
@@ -1,7 +1,9 @@
+setwd("/Users/anttonalberdi/github/holo-omics.github.io")
+
 #Install required libraries
-install.packages("bookdown")
-install.packages("htmlwidgets")
-install.packages("webshot")
+#install.packages("bookdown")
+#install.packages("htmlwidgets")
+#install.packages("webshot")
 
 #Load required linbra
 library(bookdown)
@@ -9,7 +11,7 @@ library(htmlwidgets)
 library(webshot)
 
 #Install install_phantomjs
-webshot::install_phantomjs()
+#webshot::install_phantomjs()
 
 #Render it as github pages ()
 render_book(input = "index.Rmd", output_format = "bookdown::gitbook")
@@ -26,3 +28,9 @@ render_book(input = "index.Rmd", output_format = "bookdown::pdf_book")
 
 https://bookdown.org/yihui/rmarkdown-cookbook/
 https://bookdown.org/yihui/bookdown/
+
+######
+# For getting bib data from dois
+######
+
+https://www.doi2bib.org/
